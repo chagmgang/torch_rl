9b72cd1fa436af94b010f1f8cc9020fde1f52413
chagmgang <chagmgang@gmail.com>
2018-11-01 11:32:21 +0900

    add

---
+++ Trust_Region_Policy_Optimization/main.py
@@ -0,0 +1,31 @@
+import gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.nn.init as init
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.actor_layer_1 = nn.Linear(4, 60)
+        self.actor_layer_2 = nn.Linear(60, 60)
+        self.actor_layer_3 = nn.Linear(60, 1)
+        
+        self.critic_layer_1 = nn.Linear(4, 60)
+        self.critic_layer_2 = nn.Linear(60, 60)
+        self.critic_layer_3 = nn.Linear(60, 1)
+
+    def forward(self, x):
+        actor = F.relu(self.actor_layer_1(x))
+        actor = F.relu(self.actor_layer_2(actor))
+        actor = F.softmax(self.actor_layer_3(actor))
+
+        critic = F.relu(self.critic_layer_1(x))
+        critic = F.relu(self.critic_layer_2(critic))
+        critic = self.critic_layer_3(critic)
+
+        return actor, critic
